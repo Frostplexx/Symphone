c851d0a45a387769230ee09777f803e5a1e3b24b
Frostplexx <62436912+Frostplexx@users.noreply.github.com>
2021-08-02 17:13:17 +0200

    delete old files

--- src/getSongs.js
+++ src/getSongs.js
@@ -12,15 +12,16 @@ spotifyApi.setRefreshToken(refreshToken);
 
 
 //Youtube Search stuff
-const yt = require('youtube-search-without-api-key');
+const usetube = require('usetube')
 const ytdl = require('ytdl-core');
 
-
-
 //get Playlist ID
-var playlist = "https://open.spotify.com/playlist/2HNyg8hThHjgvlzDah7sHD?si=cf47996a64124e4a";
+var playlist = "https://open.spotify.com/playlist/2HNyg8hThHjgvlzDah7sHD?si=883761e844cf4da7";
 var playlistID = playlist.split("playlist/")[1].split("?si=")[0];
 
+//ffmpeg
+var ffmpeg = require('fluent-ffmpeg');
+const { resolve } = require('path');
 
 //GET SONGS FROM PLAYLIST
 async function getPlaylistTracks(playlistId) {
@@ -32,22 +33,39 @@ async function getPlaylistTracks(playlistId) {
   let tracks = [];
   for (let track_obj of data.body.items) {
     const track = track_obj.track
+    let dir = path.join(__dirname + `/music/${track.name.replace("//", "")}.mp4`);
     tracks.push(track);
-    let video = track.name + "  " + track.artists[0].name
+    let video = track.artists[0].name + " - " + track.name
     console.log(video)
 
-    /**
-     * Given a search query, searching on youtube
-     * @param {string} search value.
-     */
-    const videos = await yt.search(video);
-    console.log(videos[0]);
-    if(videos[0] != undefined)
-      ytdl(videos[0].url).pipe(fs.createWriteStream(__dirname + `/music/${track.name.replace("//","")}.mp4`));
+    //search video
+    let videos = await usetube.searchVideo(video);
+    //download it
+    var y = ytdl('http://www.youtube.com/watch?v=' + videos.videos[0].id).pipe(fs.createWriteStream(dir));
+
+    //when the download finishes convert the file and delete the old one
+    y.on("close", function(){
+      ffmpeg(dir)
+      .toFormat('mp3')
+      .on('error', (err) => {
+          console.log('An error occurred: ' + err.message);
+      })
+      .on('progress', (progress) => {
+          console.log('Processing: ' + progress.targetSize + ' KB converted');
+      })
+      .on('end', () => {
+          console.log('Processing finished !');
+          //delete old file
+          fs.unlinkSync(dir);
+      })
+      .save(__dirname + `/music/${track.name}.mp3`);//path where you want to save your file
+    });
   }
   return tracks;
   }
   getPlaylistTracks(playlistID);
+
+
 
   function readSettings(setting) {
     let rawdata = fs.readFileSync(path.join(__dirname, "settings.json"));
--- src/scripts/update.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-import os
-import sys
-
-arguments = sys.argv
-
-subprocess.call([arguments[1]])
-os.remove(arguments[1])
